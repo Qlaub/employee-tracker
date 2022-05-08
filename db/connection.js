33c448f2209d028c358bd3cbd74eb4c9a26a38ca
Alex Glaubitz <e.pirazzi@gmail.com>
2022-05-08 19:59:58 -0400

    routes created, db connected

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    // rename .env_sample to .env and within that file update the DB_PASSWORD to equal your mysql password
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  },
+  console.log('Connected to the company database.')
+);
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 const init = require('./utils/userChoices');
+const express = require('express');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+
+// start server after db connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
 
 init();
